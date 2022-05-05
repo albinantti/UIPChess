8206a5f4cde7ad428df5c00b87073d47f3097a21
Albin Antti <albin_antti@hotmail.com>
2022-05-05 11:24:37 +0200

    Implement exit button for main menu and game

--- Project/Exit Button.gd
+++ Project/Exit Button.gd
@@ -9,6 +9,8 @@ extends Button
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	self.get_node("ExitConfirmation").get_ok().connect("pressed", self, "confirmed")
+	self.get_node("ExitConfirmation").get_close_button().hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,4 +19,8 @@ func _ready():
 
 
 func _pressed():
+	self.get_node("ExitConfirmation").popup_centered_minsize()
+	
+	
+func confirmed():
 	get_tree().quit()
---
+++ Project/ExitActiveGameButton.gd
@@ -0,0 +1,15 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.get_node("ExitConfirmationPopup").get_ok().connect("pressed", self, "confirmed")
+	self.get_node("ExitConfirmationPopup").get_close_button().hide()
+
+
+func _pressed():
+	self.get_node("ExitConfirmationPopup").popup_centered_minsize(Vector2( 200.0, 100.0 ))
+
+
+func confirmed():
+	get_tree().change_scene("res://mainMenu.tscn")
