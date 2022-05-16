0e3ab932611ae173ca3ad8c2d5b6115e33221400
Albin Antti <albin_antti@hotmail.com>
2022-05-16 14:57:46 +0200

    Implement settings, tutorial and load game menu (#43)
    * Add translations for settings
    
    * Add settings menu
    
    * Update theme structures
    
    * Add tutorial panel
    
    * Add load game menu
    
    * Hide load panel
    
    * Fix again hihi

---
+++ Project/LoadGameLoadButton.gd
@@ -0,0 +1,30 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.get_node("../LoadedFileLabel").set_text(tr("LOAD_GAME_LOADED_FILE").format({file=tr("CONSTANT_NONE")}))
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_LoadGameSelectFileButton_pressed():
+	self.get_node("LoadGameFileDialog").popup_centered()
+
+
+
+func _on_LoadGameFileDialog_file_selected(path):
+	self.get_node("../LoadedFileLabel").set_text(tr("LOAD_GAME_LOADED_FILE").format({file=tr(path)}))
+	self.disabled = false
+	
+func _pressed():
+	get_tree().change_scene("res://GameScene.tscn")
