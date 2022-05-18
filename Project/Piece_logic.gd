16b25ddc45862acd670fffc5852046c3f5be2511
Maja4713 <57525721+Maja4713@users.noreply.github.com>
2022-05-18 10:39:12 +0200

    Piece movement (#44)
    * changed alla squares
    
    * collisionshape on squares
    
    * connecting
    
    * Fix movement with signals
    
    * Refactor pieces placement
    
    * Refactor placement of all pieces
    
    * movement remake
    
    Co-authored-by: Daniel Fehrm <daniel@fehrm.se>

---
+++ Project/Piece_logic.gd
@@ -0,0 +1,44 @@
+extends Area2D
+
+signal piece_chosen(piece)
+
+var velocity = Vector2()
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _process(state):
+	pass
+#func move():
+#	if Input.is_action_pressed('ui_right'):
+#		apply_central_impulse(Vector2(0, 10))
+	
+#func _on_Area2D_input_event(viewport, event, shape_idx):
+#	if  event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
+#		print("lol it works")
+#		print(self.name)
+#		emit_signal('piece_chosen', self.name)
+
+func _move_piece(new_position, chosen_piece):
+	if chosen_piece.name == self.name:
+		var TweenNode = get_node("Tween")
+		TweenNode.interpolate_property(
+			self, "position", get_position(), new_position, 
+			2, Tween.TRANS_EXPO, Tween.EASE_IN_OUT
+		)
+		TweenNode.start()
+
+
+func _input_event(viewport, event, shape_idx):
+	if  event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
+		emit_signal('piece_chosen', self)
