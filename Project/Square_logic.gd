3b29a440d389e6e3579000e2b5b8b400189ead26
Albin Antti <albin_antti@hotmail.com>
2022-05-24 13:23:39 +0200

    Add history panel content

--- Project/Square_logic.gd
+++ Project/Square_logic.gd
@@ -16,19 +16,18 @@ func _set_collisionshape_size():
 	var shape = collisionShape.get_shape()
 	var rect_name = self.name + "_rect"
 	var rect = self.get_node(rect_name)
-	
-	var size = (rect.get_size())/2 
+
+	var size = (rect.get_size())/2
 	shape.set_extents(Vector2(size)) # set the size of the collsionshape as the rectangle
 	collisionShape.set_shape(shape)
-	
+
 	# position is relative to the area2D and the origin of the collisionshape is in the center
 	collisionShape.set_position(Vector2(size))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-		
+
 func _input_event(viewport, event, shape_idx):
 	if  event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
-		print("random text")
 		emit_signal("square_chosen", self)
